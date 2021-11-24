3851e8379e519fffa23f93c3149d80d81232a3df
coffmanjrp <coffmanjrp@gmail.com>
2021-11-23 17:52:41 -0800

    Test fetching data with redis cache

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const Redis = require('redis');
+const redisClient = Redis.createClient();
+
+const DEFAULT_EXPIRATION = 3600;
 
 const app = express();
 app.use(cors());
 
 app.get('/photos', async (req, res) => {
   const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    'https://jsonplaceholder.typicode.com/photos',
-    { params: { albumId } }
-  );
-  res.json(data);
+
+  redisClient.get(`photos?albumId=${albumId}`, async (error, photos) => {
+    if (error) console.error(error);
+    if (photos != null) {
+      console.log('Chache Hit');
+      return res.json(JSON.parse(photos));
+    } else {
+      console.log('Chache Miss');
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/photos',
+        { params: { albumId } }
+      );
+      redisClient.setex(
+        `photos?albumId=${albumId}`,
+        DEFAULT_EXPIRATION,
+        JSON.stringify(data)
+      );
+      res.json(data);
+    }
+  });
 });
 
 app.get('/photos/:id', async (req, res) => {
